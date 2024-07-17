9f093a526007a00a8aad60efbe880fd309efb1b1
aybeniz <aybenizkazimova15@gmail.com>
2024-07-17 20:44:18 +0400

    commit

---
+++ src/main/java/az/edu/turing/turingtinderapp/TuringTinderAppApplication.java
@@ -0,0 +1,13 @@
+package az.edu.turing.turingtinderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuringTinderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuringTinderAppApplication.class, args);
+    }
+
+}
