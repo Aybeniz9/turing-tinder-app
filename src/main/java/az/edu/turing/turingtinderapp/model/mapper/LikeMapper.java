f881e72d0820a8f708c673d7d77e1c7bd2262c96
ilhammuradov <muradovilham039@gmail.com>
2024-07-19 15:50:34 +0400

    Mapper adjustments

--- src/main/java/az/edu/turing/turingtinderapp/model/mapper/LikeMapper.java
+++ src/main/java/az/edu/turing/turingtinderapp/model/mapper/LikeMapper.java
@@ -4,8 +4,12 @@ import az.edu.turing.turingtinderapp.domain.entity.Like;
 import az.edu.turing.turingtinderapp.model.dto.LikeDto;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface LikeMapper {
     LikeDto toDto(Like like);
     Like toEntity(LikeDto likeDto);
+    List<LikeDto> toDtoList(List<Like> likes);
+    List<Like> toEntityList(List<LikeDto> likeDtos);
 }
